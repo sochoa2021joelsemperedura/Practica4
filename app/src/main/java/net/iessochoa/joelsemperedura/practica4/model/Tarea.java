1781ed43a3cbcc62d17c0206ec49e83f0f14dd97
Joel Sempere <joelsempere96@gmail.com>
2021-11-01 15:32:33 +0100

    POJO parcelable

---
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica4/model/Tarea.java
@@ -0,0 +1,156 @@
+package net.iessochoa.joelsemperedura.practica4.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Objects;
+
+/*
+POJO que mantendra los datos de cada tarea
+ */
+public class Tarea implements Parcelable {
+    //contador de tareas
+    static int contador=1;
+    private int id;
+    private String prioridad;
+    private String categoria;
+    private String estado;
+    private String tecnico;
+    private String descripcion;
+    private String resumen;
+
+    //***********************CONSTRUCTORES******************************//
+    public Tarea(int id, String prioridad, String categoria, String estado, String tecnico, String descripcion, String resumen) {
+        this.id = id;
+        this.prioridad = prioridad;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.tecnico = tecnico;
+        this.descripcion = descripcion;
+        this.resumen = resumen;
+    }
+
+    public Tarea(String prioridad, String categoria, String estado, String tecnico, String descripcion, String resumen) {
+        this.id=contador++;
+        this.prioridad = prioridad;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.tecnico = tecnico;
+        this.descripcion = descripcion;
+        this.resumen = resumen;
+    }
+
+    //****************************GETTER*Y*SETTER***********************//
+    public static int getContador() {
+        return contador;
+    }
+    public static void setContador(int contador) {
+        Tarea.contador = contador;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPrioridad() {
+        return prioridad;
+    }
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getTecnico() {
+        return tecnico;
+    }
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getResumen() {
+        return resumen;
+    }
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    //************************EQUALS****************************//
+    /**
+     * Creamos el método equals que nos permitirá localizar fácilmente el objeto en un arraylist mediante indexOf
+     * Dos notas son iguales si lo es su id
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarea tarea = (Tarea) o;
+        return id == tarea.id && Objects.equals(prioridad, tarea.prioridad) && Objects.equals(categoria, tarea.categoria) && Objects.equals(estado, tarea.estado) && Objects.equals(tecnico, tarea.tecnico) && Objects.equals(descripcion, tarea.descripcion) && Objects.equals(resumen, tarea.resumen);
+    }
+
+    //***********************PARCELABLE******************************//
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.prioridad);
+        dest.writeString(this.categoria);
+        dest.writeString(this.estado);
+        dest.writeString(this.tecnico);
+        dest.writeString(this.descripcion);
+        dest.writeString(this.resumen);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.id = source.readInt();
+        this.prioridad = source.readString();
+        this.categoria = source.readString();
+        this.estado = source.readString();
+        this.tecnico = source.readString();
+        this.descripcion = source.readString();
+        this.resumen = source.readString();
+    }
+
+    protected Tarea(Parcel in) {
+        this.id = in.readInt();
+        this.prioridad = in.readString();
+        this.categoria = in.readString();
+        this.estado = in.readString();
+        this.tecnico = in.readString();
+        this.descripcion = in.readString();
+        this.resumen = in.readString();
+    }
+
+    public static final Creator<Tarea> CREATOR = new Creator<Tarea>() {
+        @Override
+        public Tarea createFromParcel(Parcel source) {
+            return new Tarea(source);
+        }
+
+        @Override
+        public Tarea[] newArray(int size) {
+            return new Tarea[size];
+        }
+    };
+}

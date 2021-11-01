1bd49d80ea37565479f6b7f4bede19d36482632d
Joel Sempere <joelsempere96@gmail.com>
2021-11-01 12:14:16 +0100

    Menu sin funcionalidad hecho y visible en la actividad

--- app/src/main/java/net/iessochoa/joelsemperedura/practica4/MainActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica4/MainActivity.java
@@ -3,6 +3,9 @@ package net.iessochoa.joelsemperedura.practica4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //*************************MENU****************************//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    //Opciones de los items del menu. TODO las acciones reales, actualmente solo muestran un "print"
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Toast.makeText(this, R.string.stAnyadir,Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_del:
+                Toast.makeText(this, R.string.stBorrar,Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_acercade:
+                Toast.makeText(this, R.string.stAcercaDe,Toast.LENGTH_SHORT).show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

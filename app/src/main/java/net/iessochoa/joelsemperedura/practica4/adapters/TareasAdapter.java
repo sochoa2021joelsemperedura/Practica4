bec9d9410a6285fdaa044e972a8f48971139e4d5
JoelS <joelsempere96@gmail.com>
2021-11-04 17:51:43 +0100

    Trabajando con el recicledView, haciendo el adapter

--- app/src/main/java/net/iessochoa/joelsemperedura/practica4/adapters/TareasAdapter.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica4/adapters/TareasAdapter.java
@@ -1,4 +1,31 @@
 package net.iessochoa.joelsemperedura.practica4.adapters;
 
-public class TareasAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TareasAdapter extends RecyclerView.Adapter<TareasAdapter.TareaViewHolder> {
+    @NonNull
+    @Override
+    public TareaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TareaViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class TareaViewHolder extends RecyclerView.ViewHolder {
+        public TareaViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
